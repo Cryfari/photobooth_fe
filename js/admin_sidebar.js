a5aaf58cc9246652b84b883ebb020942b7a1005a
Pahrijal Saban Mubarok <kevinsudrajat27@gmail.com>
2025-08-27 14:36:02 +0700

    update

--- js/admin_sidebar.js
+++ js/admin_sidebar.js
@@ -166,6 +166,13 @@ const createSidebar = (activePage = '') => {
                             <span>Composed Gallery</span>
                         </a>
                     </li>
+                    <!-- [BARU] Tautan ke galeri generated -->
+                    <li>
+                        <a href="${base_path}admin_generated_gallery.html" class="${activePage === 'generated_gallery' ? 'active' : ''}">
+                            <svg viewBox="0 0 24 24"><path fill="currentColor" d="M19 3H5c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h14c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2zm-4.83 12.67l-2.17-2.17-2.17 2.17-1.41-1.41 2.17-2.17-2.17-2.17 1.41-1.41 2.17 2.17 2.17-2.17 1.41 1.41-2.17 2.17 2.17 2.17-1.41 1.41z"/></svg>
+                            <span>Generated Gallery</span>
+                        </a>
+                    </li>
                     <li>
                         <a href="${base_path}admin_manage_packages.html" class="${activePage === 'packages' ? 'active' : ''}">
                             <svg viewBox="0 0 24 24"><path d="M21.41 11.58l-9-9C12.05 2.22 11.55 2 11 2H4c-1.1 0-2 .9-2 2v7c0 .55.22 1.05.59 1.42l9 9c.36.36.86.58 1.41.58c.55 0 1.05-.22 1.41-.59l7-7c.37-.36.59-.86.59-1.41c0-.55-.23-1.06-.59-1.42zM5.5 7C4.67 7 4 6.33 4 5.5S4.67 4 5.5 4S7 4.67 7 5.5S6.33 7 5.5 7z"/></svg>
